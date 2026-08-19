39c933cb69323c77b0cb29ae90f96e292f9caac4
utsavanand226-jpg <utsavanand226@gmail.com>
2026-08-19 18:35:42 +0530

    Add program to print day of the week based on input

---
+++ day10_code2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    printf("Enter a number (1-7): ");
+    scanf("%d", &n);
+    switch (n) {
+    case 1:
+        printf("Monday\n");
+        break;
+    case 2:
+        printf("Tuesday\n");
+        break;
+    case 3:
+        printf("Wednesday\n");
+        break;
+    case 4:
+        printf("Thursday\n");
+        break;
+    case 5:
+        printf("Friday\n");
+        break;
+    case 6:
+        printf("Saturday\n");
+        break;
+    case 7:
+        printf("Sunday\n");
+        break;
+    default:
+        printf("Invalid day number\n");
+    }
+    return 0;
+}
