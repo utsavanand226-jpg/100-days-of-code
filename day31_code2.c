91b5fe02138fce081334cb1fcf5ad63018fcdfca
utsavanand226-jpg <utsavanand226@gmail.com>
2026-09-09 18:40:42 +0530

    Implement array reversal and input/output

---
+++ day31_code2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int start = 0, end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d%c", arr[i], (i == n - 1) ? '\n' : ' ');
+    }
+    return 0;
+}
