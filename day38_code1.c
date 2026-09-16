641880955597967aeab6f2d1766461540ef73418
utsavanand226-jpg <utsavanand226@gmail.com>
2026-09-16 18:24:15 +0530

    Implement matrix addition in day38_code1.c

---
+++ day38_code1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    if (scanf("%d %d", &rows, &cols) != 2) return 0;
+
+    int a[rows][cols], b[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d%c", a[i][j] + b[i][j], (j == cols - 1) ? '\n' : ' ');
+        }
+    }
+
+    return 0;
+}
